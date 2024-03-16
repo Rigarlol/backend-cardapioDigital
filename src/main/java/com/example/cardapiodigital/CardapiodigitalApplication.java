5e7c910796ed54e749449890ad0e7cdd89dba7d0
Rigarlol <xrigarxd@gmail.com>
2024-03-16 10:24:07 -0300

    primeiro commit

---
+++ src/main/java/com/example/cardapiodigital/CardapiodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapiodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiodigitalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/cardapiodigital/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.cardapiodigital.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+}
